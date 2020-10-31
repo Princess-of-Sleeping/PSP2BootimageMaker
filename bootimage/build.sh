27aed5c63e61c2cc40e86674942f434d77002010
Princess-of-Sleeping <29831892+Princess-of-Sleeping@users.noreply.github.com>
2020-10-31 21:04:16 +0900

    Optimize build.sh

--- bootimage/build.sh
+++ bootimage/build.sh
@@ -1,10 +1,16 @@
 
 include(){
-  rm ../src/$2.h
+  rm ../src/modules/$2.h
   cp ./$1/$2.velf ../modules/$2.velf
-  xxd -i ../modules/$2.velf > ../src/$2.h
+  xxd -i ../modules/$2.velf > ../src/modules/$2.h
   rm ../modules/$2.velf
 }
+
+if [ ! -d src/modules ]; then
+  cd src
+  mkdir modules
+  cd ../
+fi
 
 if [ ! -d build ]; then
   mkdir build
